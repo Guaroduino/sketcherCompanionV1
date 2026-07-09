c8494499cece4cc7468a16fb017d350c66f16c83
Guaroduino <Guaroduino@gmail.com>
2026-07-09 19:31:56 -0400

    fix

--- app/src/main/java/com/sketcher/sketchercompanionv1/ui/dialogs/ImageEditDialog.kt
+++ app/src/main/java/com/sketcher/sketchercompanionv1/ui/dialogs/ImageEditDialog.kt
@@ -15,6 +15,7 @@ import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AspectRatio
