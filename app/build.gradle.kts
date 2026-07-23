e45e8f30adae6b4c9753499a53e7512808a2824c
Guaroduino <Guaroduino@gmail.com>
2026-07-23 16:37:05 -0400

    Bump version to 1.0.131 for Firebase OTA update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.sketcher.sketchercompanionv1"
         minSdk = 26
         targetSdk = 36
-        versionCode = 131
-        versionName = "1.0.130"
+        versionCode = 132
+        versionName = "1.0.131"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
